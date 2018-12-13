fea1885cbc0f4612fc4d4ec768a5d0aeeeabdab8
Edgar Ohanyan <edgarohanyan@gmail.com>
2018-12-13 17:57:19 +0400

    Change ChatServer

--- common/src/main/java/am/aca/common/ChatServer.java
+++ common/src/main/java/am/aca/common/ChatServer.java
@@ -4,7 +4,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ChatServer extends Remote {
-    void connect(String name, ChatClient c) throws RemoteException;
+    void connect(String username, ChatClient c) throws RemoteException;
 
     void disconnect(ChatClient c) throws RemoteException;
+
+    void broadcast(String clientName, String message) throws RemoteException;
 }
