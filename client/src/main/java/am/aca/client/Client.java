25236fef51090a1518c37b116a37dfdfbbc06c69
Edgar Ohanyan <edgarohanyan@gmail.com>
2018-12-13 17:32:45 +0400

    Client

--- client/src/main/java/am/aca/client/Client.java
+++ client/src/main/java/am/aca/client/Client.java
@@ -1,7 +1,32 @@
 package am.aca.client;
 
-public class Client {
-    public static void main(String[] args) {
+import am.aca.common.ChatServer;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+public class Client {
+    private static final int PORT = 1099;
+    private static final String BINDING_NAME = "RMI_chat";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Your name: ");
+        String name = scanner.nextLine().trim();
+
+        try {
+            ChatServer chatServer = (ChatServer) Naming.lookup(BINDING_NAME);
+            new Thread(new ChatClientImpl(chatServer,name));
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
