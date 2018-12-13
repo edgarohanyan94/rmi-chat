be4210783db4a2890562a607264977c3bfd847f5
Edgar Ohanyan <edgarohanyan@gmail.com>
2018-12-13 17:01:33 +0400

    ChatClient interface

--- common/src/main/java/am/aca/common/ChatClient.java
+++ common/src/main/java/am/aca/common/ChatClient.java
@@ -1,4 +1,10 @@
 package am.aca.common;
 
-public interface ChatClient {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ChatClient extends Remote {
+    void update(String name, String message) throws RemoteException;
+
+    String getName() throws RemoteException;
 }
