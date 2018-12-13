61052e990135949ab62abeae6a46bd80738c891d
Edgar Ohanyan <edgarohanyan@gmail.com>
2018-12-13 16:52:16 +0400

    Server

--- server/src/main/java/am/aca/server/Server.java
+++ server/src/main/java/am/aca/server/Server.java
@@ -1,7 +1,26 @@
 package am.aca.server;
 
-public class Server {
-    public static void main(String[] args) {
 
+import am.aca.common.ChatServer;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+
+    private static final int PORT = 1099;
+    private static final String BINDING_NAME = "RMI_chat";
+
+    public static void main(String[] args) {
+        try {
+            ChatServer chatServer = new ChatServerImpl();
+
+            Registry registry = LocateRegistry.createRegistry(PORT);
+            registry.rebind(BINDING_NAME, chatServer);
+            System.out.println("Server has started");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
